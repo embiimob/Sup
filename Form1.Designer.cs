6083eb1b9784fc13cab8baba965c94f988cc4c76
embiimob <embii@embii.com>
2022-12-28 13:51:18 -0600

    levelDB form with put get and delete functions - experimenting with different key value pairs...

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,9 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.btnPut = new System.Windows.Forms.Button();
+            this.btnGet = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
             this.txtPutKey = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -41,47 +41,52 @@
             this.txtPutValue = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.txtGetValue = new System.Windows.Forms.TextBox();
+            this.lbTableName = new System.Windows.Forms.ListBox();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // btnPut
             // 
-            this.button1.Location = new System.Drawing.Point(45, 64);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Put";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnPut.Location = new System.Drawing.Point(703, 53);
+            this.btnPut.Name = "btnPut";
+            this.btnPut.Size = new System.Drawing.Size(75, 23);
+            this.btnPut.TabIndex = 0;
+            this.btnPut.Text = "Put";
+            this.btnPut.UseVisualStyleBackColor = true;
+            this.btnPut.Click += new System.EventHandler(this.btnPut_Click);
             // 
-            // button2
+            // btnGet
             // 
-            this.button2.Location = new System.Drawing.Point(45, 124);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "get";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnGet.Location = new System.Drawing.Point(703, 254);
+            this.btnGet.Name = "btnGet";
+            this.btnGet.Size = new System.Drawing.Size(75, 23);
+            this.btnGet.TabIndex = 1;
+            this.btnGet.Text = "Get";
+            this.btnGet.UseVisualStyleBackColor = true;
+            this.btnGet.Click += new System.EventHandler(this.btnGet_Click);
             // 
-            // button3
+            // btnDelete
             // 
-            this.button3.Location = new System.Drawing.Point(45, 190);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "delete";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnDelete.Location = new System.Drawing.Point(10, 466);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
             // txtPutKey
             // 
-            this.txtPutKey.Location = new System.Drawing.Point(264, 66);
+            this.txtPutKey.Location = new System.Drawing.Point(9, 84);
+            this.txtPutKey.Multiline = true;
             this.txtPutKey.Name = "txtPutKey";
-            this.txtPutKey.Size = new System.Drawing.Size(100, 20);
+            this.txtPutKey.Size = new System.Drawing.Size(295, 165);
             this.txtPutKey.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(264, 47);
+            this.label1.Location = new System.Drawing.Point(7, 63);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(25, 13);
             this.label1.TabIndex = 4;
@@ -90,23 +95,24 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(264, 107);
+            this.label2.Location = new System.Drawing.Point(7, 268);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(24, 13);
+            this.label2.Size = new System.Drawing.Size(25, 13);
             this.label2.TabIndex = 6;
-            this.label2.Text = "key";
+            this.label2.Text = "Key";
             // 
             // txtGetKey
             // 
-            this.txtGetKey.Location = new System.Drawing.Point(264, 126);
+            this.txtGetKey.Location = new System.Drawing.Point(10, 284);
+            this.txtGetKey.Multiline = true;
             this.txtGetKey.Name = "txtGetKey";
-            this.txtGetKey.Size = new System.Drawing.Size(100, 20);
+            this.txtGetKey.Size = new System.Drawing.Size(294, 166);
             this.txtGetKey.TabIndex = 5;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(264, 171);
+            this.label3.Location = new System.Drawing.Point(92, 453);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(24, 13);
             this.label3.TabIndex = 8;
@@ -114,15 +120,15 @@
             // 
             // txtDeleteKey
             // 
-            this.txtDeleteKey.Location = new System.Drawing.Point(264, 190);
+            this.txtDeleteKey.Location = new System.Drawing.Point(95, 469);
             this.txtDeleteKey.Name = "txtDeleteKey";
-            this.txtDeleteKey.Size = new System.Drawing.Size(100, 20);
+            this.txtDeleteKey.Size = new System.Drawing.Size(684, 20);
             this.txtDeleteKey.TabIndex = 7;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(427, 48);
+            this.label4.Location = new System.Drawing.Point(320, 68);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(34, 13);
             this.label4.TabIndex = 10;
@@ -130,32 +136,62 @@
             // 
             // txtPutValue
             // 
-            this.txtPutValue.Location = new System.Drawing.Point(427, 67);
+            this.txtPutValue.Location = new System.Drawing.Point(321, 84);
+            this.txtPutValue.Multiline = true;
             this.txtPutValue.Name = "txtPutValue";
-            this.txtPutValue.Size = new System.Drawing.Size(100, 20);
+            this.txtPutValue.Size = new System.Drawing.Size(457, 164);
             this.txtPutValue.TabIndex = 9;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(430, 108);
+            this.label5.Location = new System.Drawing.Point(321, 268);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(33, 13);
+            this.label5.Size = new System.Drawing.Size(34, 13);
             this.label5.TabIndex = 12;
-            this.label5.Text = "value";
+            this.label5.Text = "Value";
             // 
             // txtGetValue
             // 
-            this.txtGetValue.Location = new System.Drawing.Point(430, 127);
+            this.txtGetValue.Location = new System.Drawing.Point(323, 284);
+            this.txtGetValue.Multiline = true;
             this.txtGetValue.Name = "txtGetValue";
-            this.txtGetValue.Size = new System.Drawing.Size(100, 20);
+            this.txtGetValue.ReadOnly = true;
+            this.txtGetValue.Size = new System.Drawing.Size(455, 166);
             this.txtGetValue.TabIndex = 11;
+            // 
+            // lbTableName
+            // 
+            this.lbTableName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTableName.FormattingEnabled = true;
+            this.lbTableName.ItemHeight = 20;
+            this.lbTableName.Items.AddRange(new object[] {
+            "PRO",
+            "COL",
+            "OBJ",
+            "LOG"});
+            this.lbTableName.Location = new System.Drawing.Point(112, 23);
+            this.lbTableName.Name = "lbTableName";
+            this.lbTableName.ScrollAlwaysVisible = true;
+            this.lbTableName.Size = new System.Drawing.Size(65, 24);
+            this.lbTableName.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(20, 23);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "select Table";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(790, 503);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lbTableName);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.txtGetValue);
             this.Controls.Add(this.label4);
@@ -166,11 +202,13 @@
             this.Controls.Add(this.txtGetKey);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtPutKey);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnGet);
+            this.Controls.Add(this.btnPut);
+            this.MaximumSize = new System.Drawing.Size(806, 542);
+            this.MinimumSize = new System.Drawing.Size(806, 542);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "levelDBTest";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -178,9 +216,9 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnPut;
+        private System.Windows.Forms.Button btnGet;
+        private System.Windows.Forms.Button btnDelete;
         private System.Windows.Forms.TextBox txtPutKey;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
@@ -191,6 +229,8 @@
         private System.Windows.Forms.TextBox txtPutValue;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox txtGetValue;
+        private System.Windows.Forms.ListBox lbTableName;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LevelDB;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace SUP
 {
@@ -17,8 +19,163 @@ namespace SUP
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void btnPut_Click(object sender, EventArgs e)
         {
+
+            if (lbTableName.SelectedItem == null) { lbTableName.SelectedIndex = 0; }
+
+
+            switch (lbTableName.SelectedItem.ToString().Trim())
+            {
+                case "PRO":
+                    var PRO = new Options { CreateIfMissing = true };
+                    using (var db = new DB(PRO, @"PRO"))
+                    {
+                        db.Put(txtPutKey.Text, txtPutValue.Text);
+                    }
+                    break;
+
+                case "COL":
+                    var COL = new Options { CreateIfMissing = true };
+                    using (var db = new DB(COL, @"COL"))
+                    {
+                        db.Put(txtPutKey.Text, txtPutValue.Text);
+                    }
+                    break;
+
+                case "OBJ":
+                    var OBJ = new Options { CreateIfMissing = true };
+                    using (var db = new DB(OBJ, @"OBJ"))
+                    {
+                        db.Put(txtPutKey.Text, txtPutValue.Text);
+                    }
+                    break;
+
+                case "LOG":
+                    var LOG = new Options { CreateIfMissing = true };
+                    using (var db = new DB(LOG, @"LOG"))
+                    {
+                        db.Put(txtPutKey.Text, txtPutValue.Text);
+                    }
+                    break;
+
+                default:
+                    MessageBox.Show("something went wrong");
+                    break;
+            }
+
+
+
+
+
+
+            
+        }
+
+        private void btnGet_Click(object sender, EventArgs e)
+        {
+
+
+            if (lbTableName.SelectedItem == null) { lbTableName.SelectedIndex = 0; }
+
+
+            switch (lbTableName.SelectedItem.ToString().Trim())
+            {
+                case "PRO":
+                    var PRO = new Options { CreateIfMissing = true };
+                    using (var db = new DB(PRO, @"PRO"))
+                    {
+                        var keyValue = db.Get(txtGetKey.Text);
+                        txtGetValue.Text = keyValue;
+
+                    }
+                    break;
+
+                case "COL":
+                    var COL = new Options { CreateIfMissing = true };
+                    using (var db = new DB(COL, @"COL"))
+                    {
+                        var keyValue = db.Get(txtGetKey.Text);
+                        txtGetValue.Text = keyValue;
+                    }
+                    break;
+
+                case "OBJ":
+                    var OBJ = new Options { CreateIfMissing = true };
+                    using (var db = new DB(OBJ, @"OBJ"))
+                    {
+                        var keyValue = db.Get(txtGetKey.Text);
+                        txtGetValue.Text = keyValue;
+                    }
+                    break;
+
+                case "LOG":
+                    var LOG = new Options { CreateIfMissing = true };
+                    using (var db = new DB(LOG, @"LOG"))
+                    {
+                        var keyValue = db.Get(txtGetKey.Text);
+                        txtGetValue.Text = keyValue;
+                    }
+                    break;
+
+                default:
+                    MessageBox.Show("something went wrong");
+                    break;
+            }
+
+
+
+
+
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+
+            if (lbTableName.SelectedItem == null) { lbTableName.SelectedIndex = 0; }
+
+
+            switch (lbTableName.SelectedItem.ToString().Trim())
+            {
+                case "PRO":
+                    var PRO = new Options { CreateIfMissing = true };
+                    using (var db = new DB(PRO, @"PRO"))
+                    {
+                        db.Delete(txtDeleteKey.Text);
+            
+                    }
+                    break;
+
+                case "COL":
+                    var COL = new Options { CreateIfMissing = true };
+                    using (var db = new DB(COL, @"COL"))
+                    {
+                        db.Delete(txtDeleteKey.Text);
+                    }
+                    break;
+
+                case "OBJ":
+                    var OBJ = new Options { CreateIfMissing = true };
+                    using (var db = new DB(OBJ, @"OBJ"))
+                    {
+                        db.Delete(txtDeleteKey.Text);
+                    }
+                    break;
+
+                case "LOG":
+                    var LOG = new Options { CreateIfMissing = true };
+                    using (var db = new DB(LOG, @"LOG"))
+                    {
+                        db.Delete(txtDeleteKey.Text);
+                    }
+                    break;
+
+                default:
+                    MessageBox.Show("something went wrong");
+                    break;
+            }
+
 
         }
     }
