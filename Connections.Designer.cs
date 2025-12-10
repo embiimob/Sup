dcecc15ebb94a431e890bb85ecefc8b6c46e88f7
embiimob <embii@embii.com>
2025-12-09 23:50:16 -0600

    Updated to 0.7.7-beta officially

--- Connections.Designer.cs
+++ Connections.Designer.cs
@@ -574,9 +574,9 @@
             this.label15.Location = new System.Drawing.Point(395, 437);
             this.label15.Name = "label15";
             this.label15.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.label15.Size = new System.Drawing.Size(198, 13);
+            this.label15.Size = new System.Drawing.Size(109, 13);
             this.label15.TabIndex = 124;
-            this.label15.Text = "Sup!? v0.7.7-beta PRE RELEASE";
+            this.label15.Text = "Sup!? v0.7.7-beta";
             this.label15.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // label14
