922484980515a42e64e4069a38652cf30e855dec
embii <embii@embii.com>
2025-12-13 23:18:56 -0600

    Update label15 text to 'Sup!? v0.7.8-beta'

--- Connections.Designer.cs
+++ Connections.Designer.cs
@@ -576,7 +576,7 @@
             this.label15.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.label15.Size = new System.Drawing.Size(109, 13);
             this.label15.TabIndex = 124;
-            this.label15.Text = "Sup!? v0.7.7-beta";
+            this.label15.Text = "Sup!? v0.7.8-beta";
             this.label15.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // label14
