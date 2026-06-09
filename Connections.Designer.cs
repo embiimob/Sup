88898b977e41f83f93805311a86d8153db9dc46c
embiimob <embii@embii.com>
2026-06-08 22:09:35 -0500

    Sup!? v0.9.0-beta Official Release

--- Connections.Designer.cs
+++ Connections.Designer.cs
@@ -292,7 +292,7 @@
             this.chkUseIpfsApiHelpers.AutoSize = true;
             this.chkUseIpfsApiHelpers.Location = new System.Drawing.Point(166, 395);
             this.chkUseIpfsApiHelpers.Name = "chkUseIpfsApiHelpers";
-            this.chkUseIpfsApiHelpers.Size = new System.Drawing.Size(135, 17);
+            this.chkUseIpfsApiHelpers.Size = new System.Drawing.Size(129, 17);
             this.chkUseIpfsApiHelpers.TabIndex = 165;
             this.chkUseIpfsApiHelpers.Text = " use IPFS API helpers";
             this.chkUseIpfsApiHelpers.UseVisualStyleBackColor = true;
@@ -587,9 +587,9 @@
             this.label15.Location = new System.Drawing.Point(395, 437);
             this.label15.Name = "label15";
             this.label15.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.label15.Size = new System.Drawing.Size(127, 13);
+            this.label15.Size = new System.Drawing.Size(109, 13);
             this.label15.TabIndex = 124;
-            this.label15.Text = "Sup!? v0.9.0";
+            this.label15.Text = "Sup!? v0.9.0-beta";
             this.label15.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // label14
