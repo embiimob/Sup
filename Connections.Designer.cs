f8d4a6cb0d7c2a88ee557e1f8427e1d9912089ae
embii <embii@embii.com>
2026-06-08 22:01:49 -0500

    Update Connections.Designer.cs

--- Connections.Designer.cs
+++ Connections.Designer.cs
@@ -589,7 +589,7 @@
             this.label15.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.label15.Size = new System.Drawing.Size(127, 13);
             this.label15.TabIndex = 124;
-            this.label15.Text = "Sup!? v0.8.3-Cthulhu";
+            this.label15.Text = "Sup!? v0.9.0";
             this.label15.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // label14
