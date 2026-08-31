bbcb4f951d4f8fbe3e55fe4b9a271143bcdee998
embiimob <embii@embii.com>
2026-08-31 07:44:53 -0500

    Sup!? v0.9.2-beta release FIX: issues with transfering handles FIX: removes memory cacheing which was slowing down build times

--- Connections.Designer.cs
+++ Connections.Designer.cs
@@ -587,9 +587,9 @@
             this.label15.Location = new System.Drawing.Point(395, 437);
             this.label15.Name = "label15";
             this.label15.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.label15.Size = new System.Drawing.Size(109, 13);
+            this.label15.Size = new System.Drawing.Size(98, 13);
             this.label15.TabIndex = 124;
-            this.label15.Text = "Sup v0.9.1-beta";
+            this.label15.Text = "Sup v0.9.2-beta";
             this.label15.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // label14
